e8eacf8af4a32061514168633f05aecd7d13ecd2
Steven Kussmaul <steve@Stevens-MacBook-Air.local>
2018-10-17 19:06:24 -0400

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+class addressBook {
+    constructor() {
+        this.contactListArray = [];
+    }
+    add(info){
+        console.log(this.contactListArray);
+        console.log(this.contactListArray);
+        this.contactListArray.push(info);
+        console.log(this.contactListArray);
+    }
+    deleteAt(deletePosition) {
+        console.log(this.contactListArray);
+        this.contactListArray.splice(deletePosition,1);
+        console.log(this.contactListArray);
+
+    }
+    print(printPosition) {
+        console.log(this.contactListArray[printPosition]);
+
+        // for (let contact of this.contactListArray) {
+        //     if (contact === printPosition) {
+        //     }
+        // }
+    }
+
+}
+
+class contact {
+    constructor(name,email,phone,relation) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.relation = relation;
+    }
+
+}
+
+let myAddressBook = new addressBook();
+console.log(myAddressBook.contactListArray);
+
+
+while(true) {
+    let Choice= prompt("would you like to add, delete print or quit?")
+
+    if (Choice === "add") {
+        let name = "joe" //prompt ("enter a name");
+        let email = "email@gmail.com" //prompt ("enter email");
+        let phone = "867-5309" //prompt ("enter a phone");
+        let relation = "uncle" //prompt ("enter a relation");
+        myAddressBook.add(new contact(name,email,phone,relation));
+
+    } else if (Choice === "delete") { 
+        let indexPositionToDelete = prompt("enter a position to delete");
+        myAddressBook.deleteAt(indexPositionToDelete);
+
+    } else if (Choice === "print") {
+        let indexPostionToPrint= prompt("enter a position to print");
+        myAddressBook.print(indexPostionToPrint);
+
+    } else if (Choice === "quit") {
+        break;
+    } else {
+        break; //Choice= prompt("would you like to add, delete print or quit?")
+    }
+}
